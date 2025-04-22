64ea6918a35e8907d96e03ede740842eeae24484
Yasmin Abisheva <156778403+yasmin846@users.noreply.github.com>
2025-04-22 22:59:08 +0500

    Delete task/bookstore_back directory

--- task/bookstore_back/api/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Books
-
-admin.site.register(Books)
-
--- task/bookstore_back/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import books_detail, books_list
-
-urlpatterns = [
-    path('books/', books_list),
-    path('books/<int:id>/', books_detail)
-]
